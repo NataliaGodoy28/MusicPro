7c7b145abd446008eaca3b526335e3a78d5dae93
WilliamMenares <wi.menares@duocuc.cl>
2023-04-23 15:39:20 -0400

    base de datos lista para usar

--- TestDjango/settings.py
+++ TestDjango/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'TestDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'musicpro',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'musicpro', # Nombre de tu base de datos
+        'USER': 'root', # Usuario de la base de datos (por defecto, 'root' en XAMPP)
+        'PASSWORD': '', # Contraseña del usuario de la base de datos (deja en blanco si no tienes contraseña en XAMPP)
+        'HOST': 'localhost', # Host de la base de datos (en XAMPP, generalmente es 'localhost')
+        'PORT': '3306', # Puerto de la base de datos de MySQL (por defecto, '3306' en XAMPP)
     }
 }
 
--- core/views.py
+++ core/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 def index(request):
     return render(request, 'core/index.html')
 
 def contador(request):
-    return render(request, 'core/contador.html')
+    registro = RegistroEntrega.objects.all()
+    datos = {
+        'registro' : registro
+    }
+    return render(request, 'core/contador.html', datos)
