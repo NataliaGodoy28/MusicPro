bcbd056c750a71b42ee3264961f5c61698b97de9
Natalia Godoy <na.godoy@duocuc.cl>
2023-04-27 13:37:57 -0400

    Merge branch 'master' of https://github.com/NataliaGodoy28/MusicPro

---
+++ core/static/core/js/index.js
@@ -0,0 +1,14 @@
+function incrementarContador(){
+    const contador = document.getElementById("contador");
+    contador.stepUp();
+}
+function decrementarContador(){
+    const contador = document.getElementById("contador");
+    contador.stepDown();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    
+    
+  });
